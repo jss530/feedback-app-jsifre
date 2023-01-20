796803aed9b4bedb0c08adba8f873eb9d8be26f1
Jacqlene <contactme@jacqlenedevelops.com>
2023-01-20 17:47:15 -0500

    Add Router, about pages

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,18 +1,54 @@
+import { v4 as uuidv4 } from 'uuid';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
+import AboutIconLink from './pages/AboutIconLink';
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
 
+    const addFeedback = (newFeedback) => {
+        //assigns unique & random IDs for each item
+        newFeedback.id = uuidv4();
+        setFeedback([newFeedback, ...feedback]);
+    }
+
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure you want to delete?')) {
+            setFeedback(feedback.filter(item => item.id !== id));
+        }
+    }
+    // for react-router version 6 or higher, need to wrap multiple routes in the Routes tag. Then, if there are several components in one route, they need
+    // to all be added in a parent tag. Note the property is called 'element' and not 'component'
     return (
-        <>
+        <Router>
             <Header />
             <div className='container'>
-                <FeedbackList feedback={feedback}/>
+                <Routes> 
+                    <Route path='/' element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback}/>
+                            <FeedbackStats feedback={feedback}/>
+                            <FeedbackList 
+                                feedback={feedback}
+                                handleDelete={deleteFeedback}
+                            />
+                        </>
+                    }>
+                    </Route>
+    
+
+                    <Route path='/about' element={<AboutPage />}/>
+                </Routes>
+
+                <AboutIconLink />
             </div>
-        </>
+        </Router>
     )
 }
 
--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,22 +1,24 @@
+import { FaTimes } from 'react-icons/fa';
 import Card from "./shared/Card";
 import PropTypes from 'prop-types';
 
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
    //rating is the name of the state, setRating is the function to update said state. Inside useState is the default 
 //   const [rating, setRating] = useState(7)
 //   const [text, setText] = useState('This is an example of a feedback item.')
 
-//   const handleClick = () => {
+//   const handleClick = (id) => {
 //     // can pass another function to this handleClick so that you can have access to the previous state and mutate it
-//     setRating((prev) => {
-//         return prev += 1;
-//     });
+//     console.log(id);
 //   }
 
 
   return (
     <Card>
         <div className="num-display">{item.rating}</div>
+        <button onClick={() => handleDelete(item.id)} className="close">
+            <FaTimes color='purple'/>
+        </button>
         <div className="text-display">{item.text}</div>
     </Card>
   )
